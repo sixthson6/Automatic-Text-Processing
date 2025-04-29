07b544d2da920b92951432bb33111da584e80a5a
sixthson6 <sudais2408@gmail.com>
2025-04-29 16:35:02 -0700

    Clean-up main class

--- src/main/java/text/processor/MainApp.java
+++ src/main/java/text/processor/MainApp.java
@@ -1,24 +1,3 @@
-// package text.processor;
-
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
-
-// public class MainApp extends Application {
-//     @Override
-//     public void start(Stage primaryStage) throws Exception {
-//         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml")));
-//         primaryStage.setTitle("Text Processing System");
-//         primaryStage.setScene(scene);
-//         primaryStage.show();
-//     }
-
-//     public static void main(String[] args) {
-//         launch(args);
-//     }
-// }
-
 package text.processor;
 
 import java.io.File;
