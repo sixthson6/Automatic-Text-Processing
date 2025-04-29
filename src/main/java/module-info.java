6090f79ce2fd214901dd14d5db52a036addace8c
sixthson6 <sudais2408@gmail.com>
2025-04-29 16:32:43 -0700

    Add module configurations

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module text.processor {
     requires javafx.controls;
     requires javafx.fxml;
+    // requires javafx.controls;
+    requires javafx.graphics;
 
     opens text.processor to javafx.fxml;
     exports text.processor;
